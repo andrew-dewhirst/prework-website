70ea20ac1589dcaaa0d90cc85693df85ca662b5d
Andrew Dewhirst <andrew.dewhirst8@gmail.com>
2021-08-18 19:39:46 -0400

    Added logo and color scheme

--- script.js
+++ script.js
@@ -1,7 +1,6 @@
 const h2 = document.createElement("h2");
-h2.textContent = "This content added by JavaScript";
+h2.textContent = "Software Engineer | Solutions Engineer | Web Developer";
+
 
 document.querySelector("body").appendChild(h2);
-
-const header = document.getElementsByClassName("links");
-
+document.querySelector("h2").style.color = "#595959";
