a3fd0c514f14b792dbdf6d439e0c6938c5b3edf7
Andrew Dewhirst <andrew.dewhirst8@gmail.com>
2021-08-19 21:32:12 -0400

    Adjusted for differing viewport sizes

--- script.js
+++ script.js
@@ -5,5 +5,5 @@ h2.textContent = "Software Engineer | Solutions Engineer | Web Developer";
 document.querySelector("body").appendChild(h2);
 document.querySelector("h1").style.color = "#595959";
 document.querySelector("h2").style.color = "#595959";
-document.querySelector("h2").style.top = "360px";
-document.querySelector("h2").style.left = "400px";
+document.querySelector("h2").style.top = "50%";
+document.querySelector("h2").style.left = "31%";
